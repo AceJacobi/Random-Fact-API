ff05d65ad1e9161ed49b6c037dc7a0383bfd34f7
James Jones <46056989+AceJacobi@users.noreply.github.com>
2022-06-09 10:51:35 -0500

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,7 +1,3 @@
-
-
 let factStore = ['Three presidents, all Founding Fathers—John Adams, Thomas Jefferson, and James Monroe—died on July 4.',"Barbie's full name is Barbara Millicent Roberts.","Robert Wadlow is the tallest man ever at 8’11”.","The Four Corners is the only spot in the US where you can stand in four states at once: Utah, Colorado, Arizona and New Mexico.", "Bats are the only mammals that can fly.", "Octopuses have three hearts.", "A blue whale heart weighs 400 pounds.", "A tiger’s skin is striped like its fur.", "Mosquitos are the deadliest animal in the world.", "There are no muscles in your fingers.", "It’s impossible to hum while holding your nose.", "Q is the only alphabet not present in the name of a state.", "Strengths is the longest one syllable world.", "Hawaiian pizza was created in Ontario Canada.", "The Burj Khalifa in Dubai is the tallest building in the world at 2717 feet.", "The teddy bear is named after President Theodore Roosevelt.", "More people visit France than any other country.", "At any given moment there are 2000 thunderstorms happening somewhere in the world.", "The windy city nickname has nothing to do with Chicago weather...", "Peanuts aren’t nuts, they’re legumes."];
-
-
 
 module.exports = factStore;
