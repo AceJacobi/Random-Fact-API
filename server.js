b3c4ea33b094673db9d33eac8fe1b2f59c6e448a
James Jones <46056989+AceJacobi@users.noreply.github.com>
2022-06-09 10:54:04 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 const express = require('express');
+
 const app = express();
 
 app.use(express.static('public'));
 
 const facts = require('./public/index');
+
 const randomize = require('./public/randomizer');
 
-
 app.get('/', (req, res)=>{
+    
     res.sendFile('index.html');
+    
 })
 
 app.get('/facts', (req, res)=>{
+    
     res.send(`Here's a random fact: ${randomize(facts)}`);
+    
 })
-
 
 const port = process.env.PORT || 8000;
 
