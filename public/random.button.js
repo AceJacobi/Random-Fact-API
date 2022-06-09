7024979ec715bd8e7e8e6f1fe6a2cb610885e1e7
James Jones <46056989+AceJacobi@users.noreply.github.com>
2022-06-09 10:52:10 -0500

    Update random.button.js

--- public/random.button.js
+++ public/random.button.js
@@ -1,18 +1,21 @@
 let display = document.getElementById('rFact');
+
 let instruct = document.getElementById('instructions');
+
 let button = document.getElementById('fact-b');
-
-
 
 async function randomFact(){
     
     let url = window.location.protocol + '//' + window.location.host + '/facts';
         
     let result = await fetch(url)
+    
     .then(data => data.text())
+    
     .then(response => display.innerHTML = response);     
     
     instruct.style.display = 'none';
+    
     button.style.display = 'none';
 }
 
